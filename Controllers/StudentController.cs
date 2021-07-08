797dbb47ae6015502b70ae1a11800df4f8519737
DINESHKUMAR <dk1981999@gmail.com>
2021-07-08 10:52:51 +0530

    tidy

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -9,7 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+ 
 namespace student_management_api.Controllers
 {
     public class StudentController : BaseApiController
